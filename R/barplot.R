77e26d29bd93b57193f09d32de109f3e07d2fd6f
cheng <chengff11@gmail.com>
2016-05-15 23:36:29 +0800

    Update barplot.R

--- R/barplot.R
+++ R/barplot.R
@@ -2,3 +2,9 @@
 mp<-barplot(x,names.arg=name.1,main="your title",
             xlab="x label",ylab="y label",col=rainbow(20))
 text(mp,x,x,pos=3)
+
+x<-c(1,2,3,4)
+name<-c("test1","test2","test3","test4")
+mp<-barplot(x,names.arg=name,main="your title",
+            xlab="x label",ylab="y label",col=rainbow(20),ylim=c(0,max(x)+1))
+text(mp,x,x,pos=3)
